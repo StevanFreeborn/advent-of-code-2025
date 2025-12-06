fcd314b5c2d9194a9ae0267f66e1a53bba18b850
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-12-06 07:36:16 -0600

    fix: uncomment test cases

--- cmd/03/bank/bank_test.go
+++ cmd/03/bank/bank_test.go
@@ -21,57 +21,57 @@ func TestFrom(t *testing.T) {
 type testCase struct {
 	numberOfBanksToTurnOn int
 	bank                  bank.Bank
-	expected              int
+	expected              int64
 }
 
 func TestJoltage(t *testing.T) {
 	testCases := []testCase{
-		// {
-		// 	numberOfBanksToTurnOn: 2,
-		// 	bank:                  bank.From("987654321111111"),
-		// 	expected:              98,
-		// },
+		{
+			numberOfBanksToTurnOn: 2,
+			bank:                  bank.From("987654321111111"),
+			expected:              98,
+		},
 		{
 			numberOfBanksToTurnOn: 2,
 			bank:                  bank.From("811111111111119"),
 			expected:              89,
 		},
-		// {
-		// 	numberOfBanksToTurnOn: 2,
-		// 	bank:                  bank.From("234234234234278"),
-		// 	expected:              78,
-		// },
-		// {
-		// 	numberOfBanksToTurnOn: 2,
-		// 	bank:                  bank.From("818181911112111"),
-		// 	expected:              92,
-		// },
-		// {
-		// 	numberOfBanksToTurnOn: 12,
-		// 	bank:                  bank.From("987654321111111"),
-		// 	expected:              987654321111,
-		// },
-		// {
-		// 	numberOfBanksToTurnOn: 12,
-		// 	bank:                  bank.From("811111111111119"),
-		// 	expected:              811111111119,
-		// },
-		// {
-		// 	numberOfBanksToTurnOn: 12,
-		// 	bank:                  bank.From("234234234234278"),
-		// 	expected:              434234234278,
-		// },
-		// {
-		// 	numberOfBanksToTurnOn: 12,
-		// 	bank:                  bank.From("818181911112111"),
-		// 	expected:              888911112111,
-		// },
+		{
+			numberOfBanksToTurnOn: 2,
+			bank:                  bank.From("234234234234278"),
+			expected:              78,
+		},
+		{
+			numberOfBanksToTurnOn: 2,
+			bank:                  bank.From("818181911112111"),
+			expected:              92,
+		},
+		{
+			numberOfBanksToTurnOn: 12,
+			bank:                  bank.From("987654321111111"),
+			expected:              987654321111,
+		},
+		{
+			numberOfBanksToTurnOn: 12,
+			bank:                  bank.From("811111111111119"),
+			expected:              811111111119,
+		},
+		{
+			numberOfBanksToTurnOn: 12,
+			bank:                  bank.From("234234234234278"),
+			expected:              434234234278,
+		},
+		{
+			numberOfBanksToTurnOn: 12,
+			bank:                  bank.From("818181911112111"),
+			expected:              888911112111,
+		},
 	}
 
 	for _, testCase := range testCases {
 		result := testCase.bank.Joltage(testCase.numberOfBanksToTurnOn)
 
-		if result != int64(testCase.expected) {
+		if result != testCase.expected {
 			t.Errorf("got %d but wanted %d", result, testCase.expected)
 		}
 	}
