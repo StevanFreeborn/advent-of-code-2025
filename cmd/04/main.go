0b74bfd821b4e7b9d1bdd6d8055cbc9694c988fd
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-12-08 05:39:47 -0600

    feat: solve day 4 part 2

--- cmd/04/main.go
+++ cmd/04/main.go
@@ -4,9 +4,11 @@ import (
 	"github.com/StevanFreeborn/advent-of-code-2025/internal/file"
 	"github.com/StevanFreeborn/advent-of-code-2025/internal/grid"
 	"github.com/StevanFreeborn/advent-of-code-2025/internal/move"
+	"github.com/StevanFreeborn/advent-of-code-2025/internal/position"
 )
 
 const PaperRollCharacter = "@"
+const EmptySpaceCharacter = "."
 
 func SolvePartOne(filePath string) int {
 	input := file.ReadAllLines(filePath)
@@ -27,5 +29,40 @@ func SolvePartOne(filePath string) int {
 			total++
 		}
 	}
+
+	return total
+}
+
+func SolvePartTwo(filePath string) int {
+	input := file.ReadAllLines(filePath)
+	g := grid.From(input)
+
+	total := 0
+
+	for {
+		positionsToRemove := []position.Position{}
+
+		for position := range g.Positions() {
+			value := g.GetValueAt(position)
+
+			if value != PaperRollCharacter {
+				continue
+			}
+
+			sameNeighbors := g.GetSameNeighborsOf(position, move.AllDirections)
+
+			if len(sameNeighbors) < 4 {
+				positionsToRemove = append(positionsToRemove, position)
+				total++
+			}
+		}
+
+		if len(positionsToRemove) == 0 {
+			break
+		}
+
+		g.SetValuesAt(positionsToRemove, EmptySpaceCharacter)
+	}
+
 	return total
 }
